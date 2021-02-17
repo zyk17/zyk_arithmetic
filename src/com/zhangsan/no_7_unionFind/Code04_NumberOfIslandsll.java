5657e8d61ea5823de93299bb03ad4231b580d565
zhangyuekun <1916467023@qq.com>
2021-02-17 14:37:12 +0800

    空降岛屿，求数量。解决很多个空白初始化很慢

--- src/com/zhangsan/no_7_unionFind/Code04_NumberOfIslandsll.java
+++ src/com/zhangsan/no_7_unionFind/Code04_NumberOfIslandsll.java
@@ -1,7 +1,6 @@
 package com.zhangsan.no_7_unionFind;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 /**
  * 空降岛屿
@@ -17,16 +16,17 @@ import java.util.List;
  */
 public class Code04_NumberOfIslandsll {
 
+    /** 自定义并查集，每次连接O(1)的时间复杂度, 初始化为O(row* col)
+     * 但是初始化时，要准备三个row*col的数组。如果row和col过大会导致初始化时间过久 */
     public static List<Integer> numbersOfIslands1(int row, int col, int[][] positions) {
         List<Integer> ans = new ArrayList<>();
-        UnionFind uf = new UnionFind(row, col);
+        UnionFind1 uf = new UnionFind1(row, col);
         for (int[] position : positions) {
             ans.add(uf.connect(position[0], position[1]));
         }
         return ans;
     }
-
-    public static class UnionFind {
+    public static class UnionFind1 {
         private int[] parent;
         private int[] size;
         private int[] help;
@@ -34,7 +34,7 @@ public class Code04_NumberOfIslandsll {
         private int col;
         private int sets;
 
-        public UnionFind(int row, int col) {
+        public UnionFind1(int row, int col) {
             this.row = row;
             this.col = col;
             int len = row * col;
@@ -111,22 +111,98 @@ public class Code04_NumberOfIslandsll {
 
     }
 
+    /** 解决上述row和col过大，导致初始化时间复杂度高的问题 */
+    public static List<Integer> numbersOfIslands2(int row, int col, int[][] positions) {
+        List<Integer> ans = new ArrayList<>();
+        UnionFind2 uf = new UnionFind2();
+        for (int[] position : positions) {
+            ans.add(uf.connect(position[0], position[1]));
+        }
+        return ans;
+    }
+    public static class UnionFind2 {
+
+        private Map<String, String> parents = new HashMap<>();
+        private Map<String, Integer> sizeMap = new HashMap<>();
+        private int sets;
+
+        private String findHead(String str) {
+            // 优化：把寻找过程中的节点的头节点指向head
+            Stack<String> stack = new Stack<>();
+            while (!str.equals(parents.get(str))) {
+                stack.push(str);
+                str = parents.get(str);
+            }
+            while (!stack.isEmpty()) {
+                parents.put(stack.pop(), str);
+            }
+            return str;
+        }
+
+        private void union(String s1, String s2) {
+            // 如果目标不存在，直接结束
+            if( !parents.containsKey(s1) ) {
+                return;
+            }
+            // 拿到大小
+            int size1 = sizeMap.get(s1), size2 = sizeMap.get(s2);
+            String headS1 = findHead(s1), headS2 = findHead(s2);
+            // 合并两个并集
+            if(size1 >= size2) {
+                parents.put(headS2, headS1);
+                sizeMap.put(headS1, size1+size2);
+                sizeMap.remove(headS2);
+            } else {
+                parents.put(headS1, headS2);
+                sizeMap.put(headS2, size1+size2);
+                sizeMap.remove(headS1);
+            }
+            sets--;
+        }
+
+        public int connect(int row, int col) {
+            String oneself = row + "_" + col;
+            if(!parents.containsKey(oneself)) {
+                parents.put(oneself, oneself);
+                sizeMap.put(oneself, 1);
+                sets++;
+
+                union((row - 1) + "_" + col, oneself);
+                union((row + 1) + "_" +  col, oneself);
+                union(row + "_" + (col -1) , oneself);
+                union(row + "_" +  (col + 1), oneself);
+            }
+            return sets;
+        }
+
+        public int sets() {
+            return sizeMap.size();
+        }
+
+    }
+
 
     public static void main(String[] args) {
         // 输入：   m:3, n:3, positions: [0,0],[0,1],[1,2],[2,1]
         // 输出：   [1,1,2,3]
-        int row = 3;
+        int row = 100000000;
         int col = 3;
+//        int row = 3;
+//        int col = 3;
         int[][] positions = {
                 {0, 0},
                 {0, 1},
                 {1, 2},
                 {2, 1}
         };
-        List<Integer> result = numbersOfIslands1(row, col, positions);
-        for (Integer r : result) {
-            System.out.println(r);
-        }
+        long s1 = System.nanoTime();
+        List<Integer> r1 = numbersOfIslands1(row, col, positions);
+        long s2 = System.nanoTime();
+        System.out.println("先初始化结果: " + r1 + "\t耗时: " + (s2 - s1) + "纳秒");
+        long s3 = System.nanoTime();
+        List<Integer> r2 = numbersOfIslands2(row, col, positions);
+        long s4 = System.nanoTime();
+        System.out.println("省略初始化结果: " + r2 + "\t耗时: " + (s4 - s3) + "纳秒");
     }
 
 }
