2111b0774cde19c326d209e10f0d1d49125bdce0
zhangyuekun <1916467023@qq.com>
2021-02-19 15:39:35 +0800

    使用递归反转栈

---
+++ src/com/zhangsan/no_9_recursion/Code04_ReverseStackByRecursive.java
@@ -0,0 +1,53 @@
+package com.zhangsan.no_9_recursion;
+
+import java.util.Stack;
+
+/**
+ * 使用递归反转栈,不使用任何额外数据结构
+ * 栈：先进后出
+ * @author zhangsan
+ * @date 2021/2/18 21:23
+ */
+public class Code04_ReverseStackByRecursive {
+
+    public static void reverseStackByRecursive(Stack<Integer> stack) {
+        if(stack == null || stack.isEmpty()) {
+            return;
+        }
+        int i = reverse(stack);
+        reverseStackByRecursive(stack);
+        stack.add(i);
+    }
+
+    private static int reverse(Stack<Integer> stack) {
+        Integer pop = stack.pop();
+        if(stack.isEmpty()) {
+            return pop;
+        }
+        int last = reverse(stack);
+        stack.add(pop);
+        return last;
+    }
+
+
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < 10; i++) {
+            stack.push(i);
+        }
+        /*while (!stack.isEmpty()) {
+            System.out.print(stack.pop() + "\t");
+        }
+        System.out.println();*/
+        System.out.println(stack);
+
+        reverseStackByRecursive(stack);
+
+        System.out.println(stack);
+        /*while (!stack.isEmpty()) {
+            System.out.print(stack.pop() + "\t");
+        }
+        System.out.println();*/
+    }
+
+}
