91818dcb144775621d4f2638d080ca3efc9b4db2
zhangyuekun <1916467023@qq.com>
2021-02-23 16:24:54 +0800

    字符串工具类

--- src/com/zhangsan/util/StringUtil.java
+++ src/com/zhangsan/util/StringUtil.java
@@ -7,12 +7,12 @@ package com.zhangsan.util;
  */
 public class StringUtil {
 
-    public static boolean isBlank(String str) {
+    public static boolean isEmpty(String str) {
         return str == null || str.length() == 0;
     }
 
-    public static boolean isNotBlank(String str) {
-        return !isBlank(str);
+    public static boolean isNotEmpty(String str) {
+        return !isEmpty(str);
     }
 
 }
