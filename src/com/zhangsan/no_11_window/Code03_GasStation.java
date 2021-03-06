b321b051cb38e9ebf5e063a9be7879bed3d50787
zhangyuekun <1916467023@qq.com>
2021-03-06 13:46:37 +0800

    加油站问题

--- src/com/zhangsan/no_11_window/Code03_GasStation.java
+++ src/com/zhangsan/no_11_window/Code03_GasStation.java
@@ -1,5 +1,7 @@
 package com.zhangsan.no_11_window;
 
+import java.util.LinkedList;
+
 /**
  * 加油站问题
  *
@@ -38,15 +40,65 @@ public class Code03_GasStation {
         return result;
     }
 
+    // 这个方法的时间复杂度O(N)，额外空间复杂度O(N)
+    public static boolean[] right2(int[] gas, int[] cost) {
+        boolean[] good = goodArray(gas, cost);
+        /*for (int i = 0; i < gas.length; i++) {
+            if (good[i]) {
+                return i;
+            }
+        }*/
+        return good;
+    }
+
+    public static boolean[] goodArray(int[] g, int[] c) {
+        int N = g.length;
+        int M = N << 1;
+        int[] arr = new int[M];
+        for (int i = 0; i < N; i++) {
+            arr[i] = g[i] - c[i];
+            arr[i + N] = g[i] - c[i];
+        }
+        for (int i = 1; i < M; i++) {
+            arr[i] += arr[i - 1];
+        }
+        LinkedList<Integer> w = new LinkedList<>();
+        for (int i = 0; i < N; i++) {
+            while (!w.isEmpty() && arr[w.peekLast()] >= arr[i]) {
+                w.pollLast();
+            }
+            w.addLast(i);
+        }
+        boolean[] ans = new boolean[N];
+        for (int offset = 0, i = 0, j = N; j < M; offset = arr[i++], j++) {
+            if (arr[w.peekFirst()] - offset >= 0) {
+                ans[i] = true;
+            }
+            if (w.peekFirst() == i) {
+                w.pollFirst();
+            }
+            while (!w.isEmpty() && arr[w.peekLast()] >= arr[j]) {
+                w.pollLast();
+            }
+            w.addLast(j);
+        }
+        return ans;
+    }
+
     // for test
     public static void main(String[] args) {
 
         int[] gas = { 1,1,3,1 };
         int[] cost = { 2,2,1,1 };
         boolean[] r = right(gas, cost);
+        boolean[] r2 = right2(gas, cost);
 
         for (int i = 0; i < r.length; i++) {
             System.out.println(r[i]);
+        }
+
+        for (int i = 0; i < r2.length; i++) {
+            System.out.println(r2[i]);
         }
     }
 
