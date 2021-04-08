59a9e3f222f5330234a2be89c6b51477f954f7cc
zhangyuekun <1916467023@qq.com>
2021-04-08 16:13:36 +0800

    门槛堆 寻找k个大元素

--- src/com/zhangsan/no_other/TopKNums.java
+++ src/com/zhangsan/no_other/TopKNums.java
@@ -57,6 +57,63 @@ public class TopKNums {
         return ans;
     }
 
+    // O(N*logK)
+    // 构建门槛, 小根堆
+    // 遍历 n-k次, 遇到比堆顶大的就加入堆, 直到最后, 堆里边全是小的内容
+    public static int[] topk4(int[] arr, int k) {
+        if (k < 1 || k > arr.length) {
+            return arr;
+        }
+        int[] kHeap = new int[k];
+        // 自底向上构建一颗k长度的堆
+        for (int i = 0; i != k; i++) {
+            heapInsert(kHeap, arr[i], i);
+        }
+        for (int i = k; i != arr.length; i++) {
+            // 从k+1开始如果比堆顶小,替换堆顶,然后向下调整堆
+            if (arr[i] > kHeap[0]) {
+                kHeap[0] = arr[i];
+                heapify(kHeap, 0, k);
+            }
+        }
+        return kHeap;
+    }
+
+    public static void heapInsert(int[] arr, int value, int index) {
+        arr[index] = value;
+        while (index != 0) {
+            int parent = (index - 1) / 2;
+            if (arr[parent] > arr[index]) {
+                swap(arr, parent, index);
+                index = parent;
+            } else {
+                break;
+            }
+        }
+    }
+
+    public static void heapify(int[] arr, int index, int heapSize) {
+        int left = index * 2 + 1;
+        int right = index * 2 + 2;
+        int largest = index;
+        while (left < heapSize) {
+            if (arr[left] < arr[index]) {
+                largest = left;
+            }
+            if (right < heapSize && arr[right] < arr[largest]) {
+                largest = right;
+            }
+            if (largest != index) {
+                swap(arr, largest, index);
+            } else {
+                break;
+            }
+            index = largest;
+            left = index * 2 + 1;
+            right = index * 2 + 2;
+        }
+    }
+
     private static void swap(int[] arr, int i, int j) {
         arr[i] = arr[i] ^ arr[j];
         arr[j] = arr[i] ^ arr[j];
@@ -84,7 +141,9 @@ public class TopKNums {
             int[] nums = ArrayUtil.generateRandomArray(i, i, true, true);
             int[] nums2 = ArrayUtil.copyArr(nums);
             int[] nums3 = ArrayUtil.copyArr(nums);
-            int k = (int) (Math.random()*i);
+            int[] nums4 = ArrayUtil.copyArr(nums);
+//            int k = (int) (Math.random()*i);
+            int k = 5;
 
             long s1 = System.currentTimeMillis();
             int[] ans1 = topk1(nums, k);
@@ -93,11 +152,14 @@ public class TopKNums {
             long s3 = System.currentTimeMillis();
             int[] ans3 = topk3(nums3, k);
             long s4 = System.currentTimeMillis();
+            int[] ans4 = topk3(nums4, k);
+            long s5 = System.currentTimeMillis();
 
             System.out.println("================size: "+ i +"=================");
-            System.out.println("排序寻找: " + (s2-s1));
-            System.out.println("通过堆: " + (s3-s2));
-            System.out.println("通过Kth: " + (s4 -s3));
+            System.out.println("排序寻找:     O(N*logN)    : " + (s2-s1));
+            System.out.println("通过大根堆:   O(N+ k*logN)  : " + (s3-s2));
+            System.out.println("通过Kth:     O(N+ k*logK)  :" + (s4 -s3));
+            System.out.println("通过门槛小根堆 O(N*logK)     :" + (s5 -s4));
             System.out.println("=================================");
             try {
                 TimeUnit.SECONDS.sleep(1);
@@ -105,11 +167,12 @@ public class TopKNums {
                 e.printStackTrace();
             }
 
-            if(!ArrayUtil.isEquals(ans1, ans2) || !ArrayUtil.isEquals(ans2, ans3)) {
+            if(!ArrayUtil.isEquals(ans1, ans2) || !ArrayUtil.isEquals(ans2, ans3) || !ArrayUtil.isEquals(ans3, ans4)) {
                 System.out.println("OOPS!");
                 ArrayUtil.printArr(ans1);
                 ArrayUtil.printArr(ans2);
                 ArrayUtil.printArr(ans3);
+                ArrayUtil.printArr(ans4);
             }
         }
 
