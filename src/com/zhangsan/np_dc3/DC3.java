0a7ad57fde172c6346927b28c77b3cb6fa58c123
zhangyuekun <1916467023@qq.com>
2021-04-18 18:39:22 +0800

    DC3算法

---
+++ src/com/zhangsan/np_dc3/DC3.java
@@ -0,0 +1,16 @@
+package com.zhangsan.np_dc3;
+
+/**
+ * DC3算法
+ *
+ * 数组,字符串.后缀数组排名
+ * @author zhangsan
+ * @date 2021/4/18 18:34
+ */
+public class DC3 {
+
+    private int[] sa;
+    private int[] rank;
+    private int[] height;
+
+}
